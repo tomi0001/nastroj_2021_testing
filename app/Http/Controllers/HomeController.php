6bf04e7091d75ec685732b40e732f84bbf60b310
tomi0001 <tomi0001@gmail.com>
2022-09-20 15:46:49 +0200

    drobna poprawka

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
         print "ddd";
     }
     /**
-     * Show the application dashboard.
+     * Show othe application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
